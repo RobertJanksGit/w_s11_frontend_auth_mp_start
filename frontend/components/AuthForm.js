15e17f68d557f77a884ffb8ed7031b847edd8893
Robert Jank <robert.jank@yahoo.com>
2024-09-10 20:10:06 -0500

    finished project

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,31 +1,72 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
-  const [message, setMessage] = useState('')
+  const [isLogin, setIsLogin] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
 
   const toggleFormMode = () => {
-    setIsLogin(!isLogin)
-    setError('')
-    setMessage('')
-  }
+    setIsLogin(!isLogin);
+    setError("");
+    setMessage("");
+  };
   const handleUsernameChange = (event) => {
-    setUsername(event.target.value)
-  }
+    setUsername(event.target.value);
+  };
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    setError("");
+    if (isLogin) {
+      try {
+        const { data } = await axios.post("/api/auth/login", {
+          username,
+          password,
+        });
+        localStorage.setItem("token", data.token);
+        navigate("/stars");
+      } catch (err) {
+        evt.preventDefault();
+        setError(
+          err?.response?.data?.message || "An error occurred. Please try again."
+        );
+      }
+    } else {
+      try {
+        const { data } = await axios.post("/api/auth/register", {
+          username,
+          password,
+        });
+        setMessage(data.message);
+        // localStorage.setItem("token", data.token);
+        navigate("/stars");
+      } catch (err) {
+        evt.preventDefault();
+        setError(
+          err?.response?.data?.message || "An error occurred. Please try again."
+        );
+      }
+    }
+  };
 
   return (
     <div className="container">
       <div aria-live="polite">{message}</div>
-      <div aria-live="assertive" style={{ color: 'red' }}>{error}</div>
-      <h3>{isLogin ? 'Login' : 'Register'}
+      <div aria-live="assertive" style={{ color: "red" }}>
+        {error}
+      </div>
+      <h3>
+        {isLogin ? "Login" : "Register"}
         <button onClick={toggleFormMode}>
-          Switch to {isLogin ? 'Register' : 'Login'}
+          Switch to {isLogin ? "Register" : "Login"}
         </button>
       </h3>
       <form>
@@ -51,8 +92,10 @@ export default function AuthForm() {
             required
           />
         </div>
-        <button type="submit">{isLogin ? 'Login' : 'Register'}</button>
+        <button type="submit" onClick={handleSubmit}>
+          {isLogin ? "Login" : "Register"}
+        </button>
       </form>
     </div>
-  )
+  );
 }
